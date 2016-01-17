501f5e1ee36b2dd00b46da18fcaeb1f6590175ba
John Ennew <johne@deeson.co.uk>
2016-01-17 10:09:35 +0000

    Added checks for aborted state as well

--- rainmaker-start.sh
+++ rainmaker-start.sh
@@ -5,11 +5,24 @@ cd $script_dir
 
 STATUS="$(vagrant status)"
 
+POWEROFF=false
 if [[ $(echo "$STATUS" | fgrep poweroff) == "" ]]; then
-    if [[ ! $(echo "$STATUS" | fgrep "The environment has not yet been created") ]]; then
-        echo 'Rainmaker is already running'
-        exit 1
-    fi
+  POWEROFF=true
+fi
+
+NEWENV=false
+if [[ $(echo "$STATUS" | fgrep "The environment has not yet been created") ]]; then
+  NEWENV=true
+fi
+
+ABORTED=true
+if [[ $(echo "$STATUS" | fgrep "The VM is in an aborted state") ]]; then
+  ABORTED=true
+fi
+
+if [[ "$NEWENV" == false && "$ABORTED" == false && "$POWEROFF" == true ]]; then
+  echo 'Rainmaker is already running'
+  exit 1
 fi
 
 vagrant up
